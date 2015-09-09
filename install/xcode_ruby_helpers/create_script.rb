f03442cd00b384b9fe6793ab9735566675e93bbf
Sergey Ilyevsky <sergey@rollout.io>
2015-09-09 18:13:21 +0300

    version 0.10.1, build 125

--- install/xcode_ruby_helpers/create_script.rb
+++ install/xcode_ruby_helpers/create_script.rb
@@ -8,7 +8,7 @@ class CreateScript
     @project = initialized_project
   end
 
-  def create_script(title, content, before_linking)
+  def create_script(title, content, position)
     @project.targets.each do |source_target| 
       if source_target.respond_to?("product_type") and source_target.product_type == "com.apple.product-type.application"
         source_build_phase = source_target.source_build_phase
@@ -17,14 +17,16 @@ class CreateScript
         script_build_phase = @project.new(Xcodeproj::Project::Object::PBXShellScriptBuildPhase)
         script_build_phase.name = title
         script_build_phase.shell_script = content
-	if before_linking
+      	if position == "before_linking"
           compile_index = build_phases.find_index { |b| b.display_name == "SourcesBuildPhase" }
-	else
+      	elsif position == "end"
           compile_index = [
-	    build_phases.find_index { |b| b.display_name == "ResourcesBuildPhase" },
-	    build_phases.find_index { |b| b.display_name == "FrameworksBuildPhase" }
-	  ].max
-	end
+      	    build_phases.find_index { |b| b.display_name == "ResourcesBuildPhase" },
+      	    build_phases.find_index { |b| b.display_name == "FrameworksBuildPhase" }
+      	  ].max
+        elsif position == "beginning"
+          compile_index = 0
+      	end
         build_phases.insert(compile_index + 1, script_build_phase) unless compile_index.nil?
       end
     end
