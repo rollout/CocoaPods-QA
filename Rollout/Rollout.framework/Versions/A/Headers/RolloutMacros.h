12d00d44fdcd66baeede073e5c67852d067b8e63
elad cohen <eladcohen@Elads-MacBook-Pro.local>
2017-03-20 16:36:09 +0200

    version 1.12.0, build 332

--- Rollout/Rollout.framework/Versions/A/Headers/RolloutMacros.h
+++ Rollout/Rollout.framework/Versions/A/Headers/RolloutMacros.h
@@ -6,5 +6,5 @@
 //  Copyright © 2017 DeDoCo. All rights reserved.
 //
 
-#define createFeatureFlag(name) @property (nonatomic, assign) BOOL is##name##Enabled;
+#define createFeatureFlag(name) @property (nonatomic, assign) RolloutFeature* is##name##Enabled;
 #define RolloutFeatureFlags [RolloutFeatureFlagsClass sharedInstance]
